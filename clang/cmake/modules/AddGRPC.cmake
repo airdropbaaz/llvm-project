37618973af9937d2b7dec23a1e7602d354460bb2
airdropbaaz <125475579+airdropbaaz@users.noreply.github.com>
2024-03-21 19:58:32 +0330

    Update AddGRPC.cmake

--- clang/cmake/modules/AddGRPC.cmake
+++ clang/cmake/modules/AddGRPC.cmake
@@ -1,7 +1,7 @@
 include(FindGRPC)
 
 function(generate_clang_protos_library LibraryName ProtoFile)
-  # Take the first two args and forward the remaining to generate_proto_sources.
+ # Take the first two args and forward the remaining to generate_proto_sources.
   cmake_parse_arguments(PARSE_ARGV 2 PROTO "" "" "")
   generate_proto_sources(ProtoSource ${ProtoFile} ${PROTO_UNPARSED_ARGUMENTS})
 
